a8266428d24f3a553b96fc71b5713d23e579621d
tlbbkoi <66895080+tlbbkoi@users.noreply.github.com>
2022-04-18 19:54:38 +0700

    Repository + UnitOfWork

---
+++ MyWebApi/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using MyWebApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<CataLog> CataLogs { get; }
+        IGenericRepository<Product> Products { get; }
+        Task Save();
+    }
+}
---
+++ MyWebApi/Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using MyWebApi.Data;
+using MyWebApi.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+
+        private IGenericRepository<CataLog> _catalogs;
+        private IGenericRepository<Product> _products;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<CataLog> CataLogs => _catalogs ??= new GenericRepository<CataLog>(_context);
+
+        public IGenericRepository<Product> Products => _products ??= new GenericRepository<Product>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
